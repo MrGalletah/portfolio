577510e5e1c7f685e49c774b84f8770c671adecc
MrGalletah <mdcabrera@hotmail.es>
2023-08-13 16:53:58 -0300

    form alert on submit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("🚧 Work in progress 🚧");
+// alert("🚧 Work in progress 🚧");
+
+window.onload = function () {
+  const button = document.getElementById("submit");
+  const name = document.getElementById("name");
+
+  button.onclick = (e) => {
+    setTimeout(() => {
+      alert(`Muchas gracias por tu mensaje ${name.value}!`);
+    }, 300);
+    e.preventDefault();
+  };
+};
